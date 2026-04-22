1b7e5479138a3a88045dbe0385d43bf6915bf016
ErkinTunc <erkintuncboya@hotmail.com>
2026-04-22 10:55:39 +0200

    Add initial project structure with Gradle configuration and Roman numeral conversion logic

---
+++ Roman-Numbers/README.md
@@ -0,0 +1,5 @@
+# Génie Logiciel L3 - TP3
+
+[Sujet](https://loriscroce.frama.io/enseignement/genie_logiciel_l3/tp3)
+
+[Guide JUnit](https://docs.junit.org/5.7.2/user-guide/)
---
+++ Roman-Numbers/app/src/main/java/org/example/Start.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Start {
+
+    // Start class
+    public static void main(String[] args) {
+
+        RomanNumber roman = new RomanNumber("IV");
+        System.out.println(roman.getValue());
+        System.out.println(roman.getRoman());
+
+    }
+
+}
